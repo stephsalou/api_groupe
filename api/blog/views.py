bc2dbd83545fb603fdf963801f10c0d1fd80938a
MJoshua25 <jyao2000@yahoo.fr>
2019-10-04 15:47:32 +0000

    add image

--- api/blog/views.py
+++ api/blog/views.py
@@ -4,3 +4,11 @@ from django_seed import Seed
 
 # Create your views here.
 seeder = Seed.seeder()
+
+Images = [os.path.join(settings.BASE_DIR,'static/images/breakfast-'+str(i)+'.jpg') for i in range(1,5)]+ [os.path.join(settings.BASE_DIR,'static/images/lunch-'+str(i)+'.jpg') for i in range(1,5)] + [os.path.join(settings.BASE_DIR,'static/images/dinner-'+str(i)+'.jpg') for i in range(1,5)] + [os.path.join(settings.BASE_DIR,'static/images/dessert-'+str(i)+'.jpg') for i in range(1,5)] + [os.path.join(settings.BASE_DIR,'static/images/drink-'+str(i)+'.jpg') for i in range(1,6)]
+
+def getimage()
+    global Images
+    img=images[randint(0,len(images)-1)]
+    img = open(img,'rb')
+    return img
