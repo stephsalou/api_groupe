6c308aba7c376e09c2bd96f4278235d2f6798850
Mamoutou <mamoutou.doumbia@uvci.edu.ci>
2019-10-04 15:58:06 +0000

    admin generator

--- api/api/settings.py
+++ api/api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'blog',
     'django_seed',
+    'django_admin_generator',
 ]
 
 MIDDLEWARE = [
--- api/blog/admin.py
+++ api/blog/admin.py
@@ -1,3 +1,125 @@
 from django.contrib import admin
 
 # Register your models here.
+# vim: set fileencoding=utf-8 :
+from django.contrib import admin
+
+from . import models
+
+
+class CategorieAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'nom',
+        'description',
+        'image',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+    list_filter = (
+        'statut',
+        'date_add',
+        'date_update',
+        'id',
+        'nom',
+        'description',
+        'image',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+
+
+class SousCategorieAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'categorie',
+        'nom',
+        'description',
+        'image',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+    list_filter = (
+        'categorie',
+        'statut',
+        'date_add',
+        'date_update',
+        'id',
+        'categorie',
+        'nom',
+        'description',
+        'image',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+
+
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'souscategorie',
+        'nom',
+        'description',
+        'image',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+    list_filter = (
+        'souscategorie',
+        'statut',
+        'date_add',
+        'date_update',
+        'id',
+        'souscategorie',
+        'nom',
+        'description',
+        'image',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+
+
+class CommentaireAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'article',
+        'user',
+        'description',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+    list_filter = (
+        'article',
+        'user',
+        'statut',
+        'date_add',
+        'date_update',
+        'id',
+        'article',
+        'user',
+        'description',
+        'statut',
+        'date_add',
+        'date_update',
+    )
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Categorie, CategorieAdmin)
+_register(models.SousCategorie, SousCategorieAdmin)
+_register(models.Article, ArticleAdmin)
+_register(models.Commentaire, CommentaireAdmin)
