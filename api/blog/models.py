6acc29a418151667c6a06706c04a75e783ffe106
Mamoutou <mamoutou.doumbia@uvci.edu.ci>
2019-10-04 15:39:34 +0000

    models

--- api/blog/models.py
+++ api/blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Categorie(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='categorie/')
+    statut = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nom
+
+class SousCategorie(models.Model):
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name='categories')
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='souscategorie/')
+    statut = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nom
+    
+class Article(models.Model):
+    souscategorie = models.ForeignKey(SousCategorie, on_delete=models.CASCADE, related_name='souscategories')
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='article/')
+    statut = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nom
+    
+class Commentaire(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='articles')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+    description = models.TextField()
+    statut = models.BooleanField(default=True)
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.username
