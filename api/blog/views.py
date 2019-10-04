cd5dab861ced5c7303d241f8ebea986ff9c0d2c2
stephsalou <49046464+stephsalou@users.noreply.github.com>
2019-10-04 15:47:14 +0000

    Update views.py

--- api/blog/views.py
+++ api/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Poll, Choice ,Vote
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from rest_framework import filters
+from rest_framework import viewsets
+
+
+class DynamicSearchFilter(filters.SearchFilter):
+    def get_search_fields(self, view, request):
+        return request.GET.getlist('search_fields', [])
+
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    filter_backends = (DynamicSearchFilter,)
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
